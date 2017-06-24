bd9698e00af15ae5122c7f14cc570f563559b4af
Chung Nguyễn <chung@Mac-Pro.local>
2017-06-24 11:00:43 +0700

    Xoay sử dụng rotare

--- src/Components/TransformView.js
+++ src/Components/TransformView.js
@@ -5,12 +5,30 @@ export default class TransformView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            colorAnim: new Animated.Value(0),            
+            colorAnim: new Animated.Value(0),
+            transform: new Animated.Value(0),
         }
     }
     componentDidMount() {        
+        // const anim1 = Animated.timing(
+        //     this.state.colorAnim,
+        //     {
+        //         toValue: 1,
+        //         duration: 1000
+        //     }
+        // );
+
+        // const anim2 = Animated.timing(
+        //     this.state.colorAnim,
+        //     {
+        //         toValue: 0,
+        //         duration: 1000
+        //     }
+        // );
+        // const finalAnim = Animated.sequence([anim1, anim2]);
+
         const anim1 = Animated.timing(
-            this.state.colorAnim,
+            this.state.transform,
             {
                 toValue: 1,
                 duration: 1000
@@ -18,21 +36,26 @@ export default class TransformView extends Component {
         );
 
         const anim2 = Animated.timing(
-            this.state.colorAnim,
+            this.state.transform,
             {
                 toValue: 0,
                 duration: 1000
             }
         );
         const finalAnim = Animated.sequence([anim1, anim2]);
+
         // Chay vong lap hien thi thay doi lien tuc
         Animated.loop(finalAnim).start();
     }
 
     render() {
-        const backgroundColor = this.state.colorAnim.interpolate({
-            inputRange: [0, 1],
-            outputRange: [ 'green', 'yellow' ]
+        // const backgroundColor = this.state.colorAnim.interpolate({
+        //     inputRange: [0, 0.5, 1],
+        //     outputRange: [ 'green', 'red', 'yellow' ]
+        // });
+        const rotate = this.state.transform.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['15deg', '0deg', '-15deg']
         });
         return (
             <View>
@@ -40,8 +63,9 @@ export default class TransformView extends Component {
                     style={{
                         width: 300,
                         height: 200,
-                        backgroundColor,
-                        marginBottom: 30
+                        backgroundColor: 'green',
+                        marginBottom: 30,
+                        transform: [ { rotate }, { rotateY: rotate } ]
                     }}>
                     
                 </Animated.View>
