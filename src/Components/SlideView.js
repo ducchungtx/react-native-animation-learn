b761dd27c2b936c7ce3baec0a3d7d85155e4e323
Chung Nguyễn <chung@Mac-Pro.local>
2017-06-24 10:31:00 +0700

    Song song nhiều animation và cấu hình chạy trước sau hoặc đồng thời.

--- src/Components/SlideView.js
+++ src/Components/SlideView.js
@@ -1,35 +1,67 @@
 import React, { Component } from 'react';
-import { Animated, Text, Easing } from 'react-native';
+import { Animated, Text, Easing, View } from 'react-native';
 
 export default class FadeView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            slideAnim: new Animated.Value(-700)
+            slideAnim1: new Animated.Value(-700),
+            slideAnim2: new Animated.Value(-700)
         }
     }
     componentDidMount() {
-        Animated.timing(
-            this.state.slideAnim,
+        const anim1 = Animated.timing(
+            this.state.slideAnim1,
             {
                 toValue: 1,
                 duration: 1000,
                 easing: Easing.ease
             }
-        ).start();
+        );
+
+        const anim2 = Animated.timing(
+            this.state.slideAnim2,
+            {
+                toValue: 1,
+                duration: 1000,
+                easing: Easing.ease
+            }
+        );
+
+        // Chay tuan tu 1 den 2
+        // Animated.sequence([anim1, anim2]).start();
+        // Chay song song 2 animation
+        // Animated.parallel([anim1, anim2]).start();
+        // Sau thoi gian bao lau thi load cai tiep theo vi du 5 giay
+        Animated.stagger(5000, [anim1, anim2]).start();
+
     }
     render() {
-        const marginLeft = this.state.slideAnim;
+        const marginLeft1 = this.state.slideAnim1;
+        const marginLeft2 = this.state.slideAnim2;
         return (
-            <Animated.View 
-                style={{
-                    width: 300,
-                    height: 200,
-                    backgroundColor: 'green',
-                    marginLeft
-                }}>
-                <Text>Chung o day</Text>
-            </Animated.View>
+            <View>
+                <Animated.View 
+                    style={{
+                        width: 300,
+                        height: 200,
+                        backgroundColor: 'green',
+                        marginBottom: 30,
+                        marginLeft: marginLeft1
+                    }}>
+                    <Text>Chung o day</Text>
+                </Animated.View>
+
+                <Animated.View 
+                    style={{
+                        width: 300,
+                        height: 200,
+                        backgroundColor: 'green',
+                        marginLeft: marginLeft2
+                    }}>
+                    <Text>Chung o day</Text>
+                </Animated.View>
+            </View>
         );
     }
 }
